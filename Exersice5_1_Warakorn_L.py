b22a5dfe2f18bffe085df21459f6f47668ab8fea
WarakornToey <warakorn_tei@hotmail.com>
2021-01-24 19:42:49 +0700

    Excerccise5_1

---
+++ Exersice5_1_Warakorn_L.py
@@ -0,0 +1,6 @@
+Number1 = int(input("Please Select Fist Number : "))
+Number2 = int(input("Please Select Last Number : "))
+print(Number1,"+",Number2,"=",Number1 + Number2)
+print(Number1,"-",Number2,"=",Number1 - Number2)
+print(Number1,"X",Number2,"=",Number1 * Number2)
+print(Number1,"/",Number2,"=",Number1 / Number2)
